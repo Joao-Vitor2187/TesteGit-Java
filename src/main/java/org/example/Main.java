03f4169331a8bb1ebfd0d015ea1bb5f0bea0fed3
Joao-Vitor2187 <javaraujo2006@gmail.com>
2026-09-13 10:12:57 -0300

    Segunda versao java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,13 @@
 package org.example;
 
 public class Main {
-    static void main() {
-        int idade = 20;
-        if (idade <= 10){
-            System.out.println("Criança");
-        } else if (idade >10 && idade <18) {
-            System.out.println("Adolescente");
-        }else{
-            System.out.println("Adulto");
-        }
-    }
+   public  static void main(String[] args){
+       int idade = 10;
+       if (idade < 10){
+           System.out.println("Ceiança");
+       } else if (idade > 10 && idade <18) {
+           System.out.println("Adolescente");
+
+       }
+   }
 }
